ab5ec4c0aa2741331603edb836dc0fd1a993fc55
NehaTanti-afk <nehatanti59@gmail.com>
2026-08-10 20:25:19 +0530

    feat: account dropdown in header

--- src/docs/Layout.tsx
+++ src/docs/Layout.tsx
@@ -1,6 +1,13 @@
 import { Link, Outlet } from "react-router";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 
 export default function Layout() {
   return (
@@ -20,6 +27,21 @@ export default function Layout() {
           >
             Theme
           </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Account
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem variant="destructive">
+                Sign out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </nav>
       </header>
       <main className="mx-auto max-w-4xl px-6 py-10">
